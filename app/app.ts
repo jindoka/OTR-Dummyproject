553230e88804d18b9cd3ccbede9c7376ec474e05
louis.heine@t-online.de <ug1t4rboy!>
2016-05-20 18:53:21 +0200

    Revert "Test"
    This reverts commit e3b3848dc2f4de18abc69c784920e6ac2a6cd6dd.

--- app/app.ts
+++ app/app.ts
@@ -4,9 +4,10 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'myapp',
-    template: '<h1>OTR JOHO</h1>'
+    template: '<h1>Angular 2 app inside a desktop app</h1>'
 })
 
 export class AppComponent {}
 
 bootstrap(AppComponent);
+
